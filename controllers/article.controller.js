3cf6722fb8a40bd5db88dbfeef33086ec01e2af7
Lada496 <67321065+Lada496@users.noreply.github.com>
2022-01-18 09:38:21 -0800

    added form

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -1,3 +1,5 @@
+const Article = require("../models/article.model");
+
 exports.getAriticles = (req, res, next) => {
   res.render("article/article-list", {
     pageTitle: "Home - All Articles",
@@ -7,4 +9,13 @@ exports.getAddArticle = (req, res, next) => {
   res.render("article/add-article", {
     pageTitle: "Add Article",
   });
+};
+
+exports.postAddArticle = async (req, res, next) => {
+  console.log(req.file);
+  const { title, image, body } = req.body;
+  if (req.file) {
+    console.log(req.file.path);
+  }
+  res.redirect("/");
 };
--- routes/article.route.js
+++ routes/article.route.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
 
 const articleController = require("../controllers/article.controller");
+// const upload = require("../util/post-article-helper").upload;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  console.log(file);
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+});
 
 router.get("/", articleController.getAriticles);
 
 router.get("/add-article", articleController.getAddArticle);
+
+router.post(
+  "/add-article",
+  upload.single("image"),
+  articleController.postAddArticle
+);
 
 module.exports = router;
